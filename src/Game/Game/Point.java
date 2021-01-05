871bf4fc073162d82fcca9d295bce7c8df5affd8
diamant <diamant.alexandre@gmail.com>
2021-01-05 14:59:31 +0100

    Enfin un vrai score

--- src/Game/Game/Point.java
+++ src/Game/Game/Point.java
@@ -4,35 +4,14 @@ public class Point {
 	static int points;
 	
 	public Point(int nbrCase) {
-		switch(nbrCase) {
-			case 2:
-				Point.points = 40;
-				break;
-			case 3:
-				Point.points = 90;
-				break;
-			case 4:
-				Point.points = 160;
-				break;
-			case 5:
-				Point.points = 250;
-				break;
-			case 6:
-				Point.points = 360;
-				break;
-			case 7 :
-				Point.points = 490;
-				break;
-			case 8 :
-				Point.points = 640;
-				break;
-			case 9 :
-				Point.points = 810;
-				break;
-			default:
-				Point.points = 1000;
+		if(nbrCase > 2){
+			for(int i = 2; i < nbrCase; i++){
+				int res = (40 * 2 + (10*i));
+				Point.points = getPoints()+res;
+			}
 		}
 	}
+	
 	public int getPoints() {
 		return points;
 	}
